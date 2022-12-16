b196a27d6072e764c3cda4c30a234067fc2d4184
srajan-kiyotaka <srajanstark.ash@gmail.com>
2022-12-16 17:22:33 +0530

    made some updates.

--- setup.py
+++ setup.py
@@ -2,22 +2,21 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='bayes_nets_sample',
-    packages = ['bayesNets'],
-    version='0.1',
-    license='MIT',
-    description = 'TYPE YOUR DESCRIPTION HERE',
-    author="Srajan Chourasia",
-    author_email='srajanstark.ash@gmail.com',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    url='https://github.com/srajan-kiyotaka/bayesNetSample',
+    name = 'bayes_nets_sample',
+    version = '0.1',
+    license = 'MIT',
+    description = 'A class to represent a Bayes Network. And apply all the different sampling strategies to the calculate a given query.',
+    author = "Srajan Chourasia",
+    author_email = 'srajanstark.ash@gmail.com',
+    packages = find_packages('src'),
+    package_dir = {'': 'src'},
+    url = 'https://github.com/srajan-kiyotaka/bayesNetSample',
     download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
-    keywords=['BayesNets'],
-    install_requires=[
+    keywords = ['BayesNets'],
+    install_requires = [
           'random',
       ],
-    classifiers=[
+    classifiers = [
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Student_Teachers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
